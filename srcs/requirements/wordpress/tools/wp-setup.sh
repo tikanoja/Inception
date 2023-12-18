a46721e630975b7b02ae2d45410053e6e00578cc
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-12-18 18:58:50 +0200

    continue from docker logs wordpress-cont

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -24,7 +24,7 @@ wp config create \
 	--dbuser=$WORDPRESS_DB_USER \
 	--dbpass=$WORDPRESS_DB_PASSWORD \
 	--dbhost=$WORDPRESS_DB_HOST \
-	--path=/var/www/html/wordpress
+	--path=/var/www/html/wordpress/
 
 # Install WordPress
 wp core install \
@@ -33,7 +33,7 @@ wp core install \
 	--admin_user=$WORDPRESS_ADMIN_USER \
 	--admin_password=$WORDPRESS_ADMIN_PASSWORD \
 	--admin_email=$WORDPRESS_ADMIN_EMAIL \
-	--path=/var/www/html/wordpress
+	--path=/var/www/html/wordpress/
 
 # Create WordPress user
 wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD
