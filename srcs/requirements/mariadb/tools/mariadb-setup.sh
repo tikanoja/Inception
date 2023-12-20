5058455a0f2d4ca46a920897b9f9d850c4194205
tikanoja <tuukka.tikanoja@gmail.com>
2023-12-20 11:23:59 +0200

    fix for script

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -15,42 +15,10 @@ chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 echo Database initialized.
 
-#TEST START!!!!
-tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
+# Run SQL setup script
+/usr/bin/mysqld --bootstrap --user=mysql < db-setup.sql
+echo Database configured.
 
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
-	cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
-CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED by '$WORDPRESS_DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';
-
-FLUSH PRIVILEGES;
-EOF
-	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-	# rm -f $tfile
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-exec /usr/bin/mysqld --user=mysql --console
-#TEST END!!!!!!
-
-# # Run SQL setup script
-# mysqld --bootstrap --datadir=/var/lib/mysql --user=mysql < ./db-setup.sql
-# echo Database configured.
-
-# # Start up MariaDB
-# echo Starting MariaDB!
-# mysqld_safe
+# Start up MariaDB
+echo Starting MariaDB!
+mysqld_safe
