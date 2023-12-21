b5dfae74033a29bff5be9e7f8b22578a10ddded2
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-12-21 11:03:43 +0200

    mariadb conf fix

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -45,10 +45,7 @@ EOF
 	# rm -f $tfile
 fi
 
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-exec /usr/bin/mysqld --user=mysql --console
+exec /usr/bin/mysqld --console --defaults-file=/etc/my.cnf.d/my-mariadb-conf.cnf
 
 # #!/bin/sh
 
