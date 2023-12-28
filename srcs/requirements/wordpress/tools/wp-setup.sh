16836bbe8791f00c4ebb843b1d052347e7f53491
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-12-28 10:51:53 +0200

    add attempt counter

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -3,8 +3,8 @@
 # Wait for MariaDB to be ready
 attempts=0
 while ! mariadb -h$MYSQL_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME &>/dev/null; do
-    echo "MariaDB unavailable. Trying again in 5 sec."
 	attempts=$((attempts + 1))
+    echo "MariaDB unavailable. Attempt $attempts: Trying again in 5 sec."
 	if [ $attempts -ge 12 ]; then
 		echo "Max attempts reached. MariaDB connection could not be established."
         exit 1
