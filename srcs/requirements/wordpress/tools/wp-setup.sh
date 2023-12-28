69c56927512eb840a457db8e16048da144c59fb0
tikanoja <tuukka.tikanoja@gmail.com>
2023-12-28 18:46:46 +0200

    update location

--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -46,8 +46,9 @@ wp config create \
 	--force
 
 # Install WordPress and feed db config
+# ADD /wordpress to URL IF NOT GOOD!!!!!
 wp core install \
-	--url=$DOMAIN_NAME/wordpress \
+	--url=$DOMAIN_NAME \
 	--title=$WORDPRESS_TITLE \
 	--admin_user=$WORDPRESS_ADMIN_USER \
 	--admin_password=$WORDPRESS_ADMIN_PASSWORD \
@@ -73,8 +74,8 @@ wp theme install inspiro \
 wp plugin update --all
 
 # Update WP address and site address to match our domain
-# wp option update siteurl "https://$DOMAIN_NAME" --allow-root
-# wp option update home "https://$DOMAIN_NAME" --allow-root
+wp option update siteurl "https://$DOMAIN_NAME" --allow-root
+wp option update home "https://$DOMAIN_NAME" --allow-root
 
 # Example setting permissions for the theme directory
 # chown -R nginx:nginx /var/www/html/wordpress/wp-content/themes/inspiro/
